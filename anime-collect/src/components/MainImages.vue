d2150387ca8104915d36841a5880450a52dec29e
Comf2 <otischaka4@gmail.com>
2022-12-14 01:47:44 -0500

    Adding Staggered Image
    Added a staggered image effect to the main images, also added a hover effect to each image

--- anime-collect/src/components/MainImages.vue
+++ anime-collect/src/components/MainImages.vue
@@ -1,54 +1,130 @@
 <template>
   <button
       class="w-20 aspect-square bg-amber-200"
-      @click="showImages()">
-    this is a button
+      >
+    this is a button {{colAmount}}
   </button>
   <div
       id="image-container"
-      class="w-full px-5 flex gap-7 flex-wrap items-center justify-center">
+      class="w-full px-5 flex justify-center">
     <div
-        v-for="imageURl in imageURLS"
-        class="">
-      <img
-          :src="`${imageURl}`"
-          alt=""
-          class="w-72"
-          loading="lazy">
+    data-purpose="image-column"
+    class="flex flex-col"
+    v-for="item in colAmount">
+      <div
+          class=" w-72 p-3 rounded-lg group relative isolate"
+          v-for="image in columns.urls[item-1]">
+        <img
+            :src="`${image}`"
+            alt=""
+            class="rounded-[inherit] group-hover:brightness-50"
+        >
+        <div
+            class=" hidden w-full h-full absolute p-7 top-0 flex-col items-end justify-between group-hover:flex "
+            >
+              <button
+                class="bg-amber-200 px-5 py-3 rounded-full ">
+                Add
+              </button>
+              <div
+                  data-purpose="low-buttons-container"
+                  class="flex justify-around gap-2"
+              >
+                <button
+                    class="w-7 aspect-square p-3 bg-white rounded-full"
+                >
+
+                </button>
+                <button
+                    class="w-7 aspect-square p-3 bg-white rounded-full"
+                >
+
+                </button>
+              </div>
+        </div>
+      </div>
+
+
     </div>
+
   </div>
 
 </template>
-
+<style>
+.image-hover-content:before{
+  content:'';
+  position: absolute;
+  inset:0;
+  background: red;
+}
+</style>
 <script>
+//TODO: create content for each column individually, refactor till desired effect is got
 import axios from 'axios';
-import {ref} from "vue";
 
 export default {
   name: 'MainImages',
   data() {
     return {
-    imageURLS : [],
+      imageURLS : [],
+
+      colAmount : this.getColAmount(),
+      rowIncrement : 3,
+      rowIncrementCount : 0,
+
+      columns : {
+        urls : [
+
+        ],
+
+      }
+
+
     }
   },
 
   methods: {
-    async showImages() {
+    initImages(){
+      for (let i = 0; i < this.colAmount; i++) {
+        this.getImages();
+      }
+      this.rowIncrementCount++;
+      if(this.rowIncrementCount < this.rowIncrement){
+        setTimeout(() => this.initImages(), 1000);
+      }
+      else this.rowIncrementCount = 0;
+    },
+
+    async getImages() {
       const Image = await axios.get('https://api.waifu.im/search/')
           .then(response => {
             let imageURL = response.data.images[0].url;
-            console.log(imageURL);
-            this.imageURLS.push(imageURL)
+            this.showImages(imageURL);
           })
           .catch(error => {
             console.log('error')
           })
     },
+
+    showImages(imageURL){
+      this.imageURLS.push(imageURL);
+      if(this.imageURLS.length >= this.rowIncrement){
+        this.columns.urls.push(this.imageURLS);
+        this.imageURLS = [];
+        console.log(this.columns.urls);
+      }
+    },
+
+    getColAmount(){
+      let columnAmount = Math.floor(window.innerWidth/288);
+      this.colAmount = columnAmount;
+      return columnAmount;
+    }
   },
   mounted() {
-    for (let i = 0; i < 8; i++) {
-      this.showImages();
-    }
+    this.initImages();
+
+    window.addEventListener('resize', this.getColAmount);
   },
 }
 
